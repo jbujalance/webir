c7ef8eaaa8625e6ebe76fe830a30a0409bf06c3e
Jose Bujalance <joseanbu23@gmail.com>
2020-04-25 16:07:59 +0200

    Setup packaging with setuptools

--- README.md
+++ README.md
@@ -2,5 +2,10 @@
 Web IR is a web server that exposes a REST-like API in order to send commands to a local LIRC daemon.
 
 ## Run locally
-In order to start the web server on a development environment, run the `run.py` file:
+In order to start the web server on a development environment, run the `run.py` file:<br>
 `python run.py`
+
+## Package
+This project is setup with `setuptools` for packaging.
+Make sure you have `wheel` installed and then use the `setup.py` script to package the project:<br>
+`python setup.py sdist bdist_wheel`
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+setup(
+    name="webir",
+    version="0.1.0",
+    author="Jose Bujalance",
+    author_email="joseab56@gmail.com",
+    description="A REST-like web interface to a local LIRC daemon",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jbujalance/webir",
+    packages=find_packages(),
+    install_requires=[
+        'Flask==1.1.2',
+        'py-irsend==1.0.2'
+    ],
+    python_requires='>=3.7',
+)
